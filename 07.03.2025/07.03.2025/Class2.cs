1e42c6aafac47e277bafd7e62c1fe40a02f97341
Egor <egorkor88900@gmail.com>
2025-03-11 14:34:14 +0500

    создал 15 классов для будущих заданий

---
+++ 07.03.2025/07.03.2025/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._03._2025
+{
+    class Class2
+    {
+    }
+}
