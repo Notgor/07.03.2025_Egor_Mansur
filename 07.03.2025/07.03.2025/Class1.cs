5c4c7bd8d3dc9f1e9a6b5c8afe967f82965fc41b
Egor <egorkor88900@gmail.com>
2025-03-11 15:39:56 +0500

    начинаю делать первый класс

--- 07.03.2025/07.03.2025/Class1.cs
+++ 07.03.2025/07.03.2025/Class1.cs
@@ -8,5 +8,17 @@ namespace _07._03._2025
 {
     class Class1
     {
+        static void Main()
+        {
+            int intValue = 42;
+            double doubleValue = 3.14159;
+            bool boolValue = true;
+            DateTime dateTimeValue = DateTime.Now;
+
+            Console.WriteLine("int: " + intValue.ToString());
+            Console.WriteLine("double: " + doubleValue.ToString());
+            Console.WriteLine("bool: " + boolValue.ToString());
+            Console.WriteLine("DateTime: " + dateTimeValue.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
     }
 }
